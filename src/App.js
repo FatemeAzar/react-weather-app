852698de0316abeb651afff372e42603c6523386
FatemeAazar <fatemeazarhazin91@gmail.com>
2022-02-07 20:15:55 +0330

    add github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        <a
+          href="https://github.com/FatemeAzar/react-weather-app"
+          target="_blank"
+        >
+          open-source{" "}
+        </a>
+        code by Fateme Azar
+      </footer>
+    </div>
+  );
 }
 
 export default App;
