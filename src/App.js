c66508e003646ae960b70300d3f22f707b246acc
FatemeAazar <fatemeazarhazin91@gmail.com>
2022-02-07 20:46:05 +0330

    added Boostrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        <a
-          href="https://github.com/FatemeAzar/react-weather-app"
-          target="_blank"
-        >
-          open-source{" "}
-        </a>
-        code by Fateme Azar
-      </footer>
+        <footer>
+          <a
+            href="https://github.com/FatemeAzar/react-weather-app"
+            target="_blank"
+          >
+            open-source{" "}
+          </a>
+          code by Fateme Azar
+        </footer>
+      </div>
     </div>
   );
 }
